0144274ffa061215a02bbc35a8167ee64212c3cf
Aakash Ramadurai B <aakash285300@gmail.com>
2026-04-15 10:59:17 +0530

    update setup.sh with github username

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 yum update -y
 yum install -y python3 python3-pip git
 cd /home/ec2-user
-git clone https://github.com/YOUR_GITHUB_USERNAME/url-shortener.git
+git clone https://github.com/Aakash-Ramadurai/url-shortener.git
 cd url-shortener
 pip3 install -r requirements.txt
 python3 app.py &
